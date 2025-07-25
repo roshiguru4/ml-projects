c156b915a3f4c38791bc088c3a48db60a25ea619
Roshan Gurumurthy <roshangurumurthy@Roshans-MacBook-Air-2.local>
2025-07-25 15:14:13 -0500

    fixed heatmap

--- obesity-random-forest/obesity.py
+++ obesity-random-forest/obesity.py
@@ -2,11 +2,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv("ml-projects/obesity-random-forest/ObesityDataSet_raw_and_data_sinthetic.csv")
+df = pd.read_csv("obesity-random-forest/ObesityDataSet_raw_and_data_sinthetic.csv")
 label_encoders = {}
 for col in df.select_dtypes(include='object').columns:
     le = LabelEncoder()
@@ -21,10 +21,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
-print("Classification Report:")
-print(classification_report(y_test, y_pred))
-conf_matrix = confusion_matrix(y_test, y_pred)
+print(f'accuracy: {accuracy_score(y_test, y_pred)}')
+print(f'confusion matrix: {confusion_matrix(y_test, y_pred)}')
+print(f'classification report: {classification_report(y_test, y_pred)}')
 plt.figure(figsize=(10, 6))
-sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues',
+sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, fmt='d', cmap='Blues',
             xticklabels=label_encoders['NObeyesdad'].classes_,
             yticklabels=label_encoders['NObeyesdad'].classes_)
+plt.show()
